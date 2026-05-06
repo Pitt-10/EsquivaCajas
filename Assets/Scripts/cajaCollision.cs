89a511a63a9ae1530b0be6c831e28cd1286dab09
Pitt-10 <pedrogutman2010@gmail.com>
2026-05-06 14:42:49 -0300

    Initial commit

---
+++ Assets/Scripts/cajaCollision.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cajaCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+	    if(col.gameObject.CompareTag("Caja"))
+	     {
+		    col.gameObject.GetComponent<cajaSpawner>().LocateBoxAtRandomPosition();	
+	     }
+    }
+}
