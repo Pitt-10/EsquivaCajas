5213d0b9fe662c9314764c7cd83f654a45636c79
Pitt-10 <pedrogutman2010@gmail.com>
2026-05-07 22:34:54 -0300

    Update playerController.cs

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+
+    float leftLimit = -2f;
+    float rightLimit = 2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,12 +16,12 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        if((Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) && transform.position.x > leftLimit)
         {
             transform.Translate(-2, 0, 0);
         }
        
-        if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        if((Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) && transform.position.x < rightLimit)
         {
             transform.Translate(2, 0, 0);
         }
